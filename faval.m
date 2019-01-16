3495560e1e4fcceaee041d21839e69b44b92dae6
MattCaner <maciejftrzcinski@gmail.com>
2019-01-16 18:07:41 +0100

    Update faval.m

--- faval.m
+++ faval.m
@@ -1,61 +1,4 @@
-iter = 1;
-%f
-    % ustawianie IW
-    for i=1:2
-        for j=1:4
-           net.IW{1,1}(i,j) = fireflies(f,iter);
-           iter = iter+1;
-        end
-    end
-    % ustawienie LW - 1/2 warstwa
-    for i=1:1
-        for j=1:2
-           net.LW{2,1}(i,j) = fireflies(f,iter); 
-           iter = iter+1;
-        end
-    end
-    % ustawienie LW - 2/3 warstwa
-    for i=2:1
-        for j=1:1
-           net.LW{3,2}(i,j) = fireflies(f,iter); 
-           iter = iter+1;
-        end
-    end  
-    % ustawienie LW - 3/4 warstwa
-    for i=1:1
-        for j=1:2
-           net.LW{4,3}(i,j) = fireflies(f,iter); 
-           iter = iter+1;
-        end
-    end     
-    % ustawienie b - 1 warstwa
-    for i=1:2
-        for j=1:1
-           net.b{1,1}(i,j) = fireflies(f,iter); 
-           iter = iter+1;
-        end
-    end   
-    % ustawienie b - 2 warstwa
-    for i=1:1
-        for j=1:1
-           net.b{2,1}(i,j) = fireflies(f,iter); 
-           iter = iter+1;
-        end
-    end   
-    % ustawienie b - 3 warstwa
-    for i=1:2
-        for j=1:1
-           net.b{3,1}(i,j) = fireflies(f,iter); 
-           iter = iter+1;
-        end
-    end   
-    % ustawienie b - 4 warstwa
-    for i=1:1
-        for j=1:1
-           net.b{4,1}(i,j) = fireflies(f,iter); 
-           iter = iter+1;
-        end
-    end   
+    net = setwb(net,fireflies(f,:));
     
     % policzenie mocy swiatla
     
@@ -67,9 +10,8 @@ iter = 1;
     
     %mse
     
-    mse = mse/L_SIZE;
-    
+    mse = mse/L_SIZE
+    fireflies_light(f)
     fireflies_light(f) = 1/mse;
-    
-    
-    
+    fireflies_light(f)
+  
