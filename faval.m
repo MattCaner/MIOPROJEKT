729cc0ea93bf855705c0f5469e93deaee5fbc50f
Heecatee <heecatee67@gmail.com>
2019-01-17 13:11:21 +0100

    adding new lightCounter

--- faval.m
+++ faval.m
@@ -7,6 +7,13 @@
         result = net(l_data(:,i));
         mse = mse + (result-l_data_correct(i))^2;
     end
+     
+    % policzenie mocy swiatla - druga wersja :)
+    %result = net(l_data);
+    %mse = 0;
+    %for i = 1:L_SIZE
+    %    mse = mse + (result(:,i)-l_data_correct(:,i))^2;
+    %end
     
     %mse
     
